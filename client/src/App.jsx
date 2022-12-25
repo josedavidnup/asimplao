bb896fb571ef331a7535d281261bb516a3fa1170
josedavidnup <josedavidnup@gmail.com>
2022-12-25 10:57:11 -0400

    client boilerplate done

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Login from './pages/auth/Login';
+import Register from './pages/auth/Register';
+import Home from './pages/Home';
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/register' element={<Register />} />
+    </Routes>
+  );
+};
+
+export default App;
